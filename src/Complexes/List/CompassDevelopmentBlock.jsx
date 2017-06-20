4b9074b0526abe7b43a4c92656aeee9cbde849d6
nikolaevigor <oldsantiago21@gmail.com>
2017-06-20 16:48:50 +0300

    Restructure project

--- src/Complexes/List/CompassDevelopmentBlock.jsx
+++ src/Complexes/List/CompassDevelopmentBlock.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Grid, Row, Col } from "react-flexbox-grid";
-import CompassDevelopmentLogo from "../img/compass-development-logo.svg";
+import CompassDevelopmentLogo from "../../img/compass-development-logo.svg";
 
 const Wrapper = styled.div`
   padding: 80px 0;
