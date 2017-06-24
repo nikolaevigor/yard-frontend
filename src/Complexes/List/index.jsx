10f2779cf148d566b38107ce37dafb8514ea52bf
Igor Nikolaev <oldsantiago21@gmail.com>
2017-06-24 12:21:13 +0300

    Reimplement Card. Pass text via props.children (#11)

--- src/Complexes/List/index.jsx
+++ src/Complexes/List/index.jsx
@@ -18,9 +18,28 @@ export default () =>
       <Intro />
       <Cards>
         <Grid>
-          <Card id={1} geo="SOUTH BEACH, SAN FRANCISCO" title="The Harrison" />
-          <Card id={2} geo="MIDTOWN EAST, MANHATTAN" title="100 East 53rd Street" />
-          <Card id={3} geo="NOLITA, MANHATTAN" title="152 Elizabeth" />
+          <Card id={1} geo="SOUTH BEACH, SAN FRANCISCO" title="The Harrison">
+            The Lewis Steel Building is a masterful industrial conversion
+            located in the heart of Williamsburg.
+            Located at 76 North 4th Street, the former 1930's steel factory
+            has been transformed into 83 individually unique and luxury loft
+            apartments.
+          </Card>
+          <Card id={2} geo="MIDTOWN EAST, MANHATTAN" title="100 East 53rd Street">
+            One Hundred East Fifty Third Street by Foster + Partners is a
+            limited collection of modern residences in Midtown Manhattan's
+            Cultural District.
+            The 94 residences ranging from alcove lofts to four bedrooms
+            within the 63-story tower are generously proportioned.
+          </Card>
+          <Card id={3} geo="NOLITA, MANHATTAN" title="152 Elizabeth">
+            152 Elizabeth is an ultra-luxury condominium building—the first in
+            New York City designed by Japanese master architect Tadao Ando.
+            Located at the corner of Kenmare and Elizabeth Streets in Nolita,
+            the 32,000-square-foot building will stand as a profound
+            architectural statement and embrace the industrial character of
+            the neighborhood.
+          </Card>
         </Grid>
       </Cards>
     </div>
