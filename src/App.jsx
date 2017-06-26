bad81fa522aca82c75df6d205248b8d0add0884e
nikolaevigor <oldsantiago21@gmail.com>
2017-06-26 16:48:22 +0300

    Add precommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Header from './Header'
-import Footer from './Footer'
+import Header from './Header';
+import Footer from './Footer';
 
-import Complexes from './Complexes/List'
-import Complex from './Complexes/Show'
+import Complexes from './Complexes/List';
+import Complex from './Complexes/Show';
 
 class App extends Component {
   render() {
