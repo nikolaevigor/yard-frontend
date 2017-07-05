a78609b0e052430eecbf8cce49fd79ec6f5039d4
Igor Nikolaev <oldsantiago21@gmail.com>
2017-07-05 16:03:12 +0300

    Add flow annotations for pluralizer

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ function getImageUrl(id: string, height: number = 512): string {
   return `https://s3-eu-central-1.amazonaws.com/yard-images/${id}-${height}`;
 }
 
-function pluralize(wordRoot, num, endings) {
+function pluralize(wordRoot: string, num: number, endings: Array<string>) {
   const cases = [0, 1, 2, 2, 2, 0, 0, 0, 0];
   return `${num} ${wordRoot}${endings[cases[num % 10]]}`;
 }
