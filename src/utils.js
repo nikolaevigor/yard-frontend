35447b1d6b84a25b2b5703c8f7a02adf8c96706a
Igor Nikolaev <oldsantiago21@gmail.com>
2017-07-05 23:22:52 +0300

    fix: inline export

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 // @flow
 
-function getImageUrl(id: string, height: number = 512): string {
+export function getImageUrl(id: string, height: number = 512): string {
   return `https://s3-eu-central-1.amazonaws.com/yard-images/${id}-${height}`;
 }
 
-function pluralize(wordRoot: string, num: number, endings: Array<string>) {
+export function pluralize(wordRoot: string, num: number, endings: Array<string>) {
   const cases = [0, 1, 2, 2, 2, 0, 0, 0, 0];
   return `${num} ${wordRoot}${endings[cases[num % 10]]}`;
 }
-
-export { getImageUrl, pluralize };
