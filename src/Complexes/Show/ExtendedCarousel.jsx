1ab0c33e8f6104079a482a012690dbde4183ff8d
nikolaevigor <oldsantiago21@gmail.com>
2017-07-11 22:51:50 +0300

    feat: finish carousel

--- src/Complexes/Show/ExtendedCarousel.jsx
+++ src/Complexes/Show/ExtendedCarousel.jsx
@@ -8,8 +8,8 @@ import { getImageUrl } from '../../utils';
 const Carousel = styled.div`
   display: flex;
   flex-flow: column;
-  align-items: center;
   position: fixed;
+  align-items: center;
   top: 0;
   left: 0;
   width: 100%;
@@ -18,9 +18,19 @@ const Carousel = styled.div`
 `;
 
 const Item = styled.img`
-  width: 790px;
-  height: 505px;
-  position: fixed;
+  justify-content: center;
+  align-items: stretch;
+  flex: 1 0;
+  margin-left: 6rem;
+  margin-right: 6rem;
+`;
+
+const ItemWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex: 0 0 100%;
+  height: 100%;
+  z-index: 100;
 `;
 
 const Counter = styled.p`
@@ -33,12 +43,13 @@ const Counter = styled.p`
   padding: 0;
 `;
 
-const Wrapper = styled.div`
+const ItemsWrapper = styled.div`
   display: flex;
   width: 100%;
   height: 100%;
   margin-top: 64px;
-  align-items: center;
+  align-items: flex-end;
+  justify-content: flex-start;
 `;
 
 const modalWindowStyle = {
@@ -47,27 +58,7 @@ const modalWindowStyle = {
   left: 0,
 };
 
-function getTranformation(idx) {
-  if (idx > 0) {
-    return {
-      transform: `translate3d( calc(50vw - 474px + ${idx * 75}vw), 0px, 0px)`,
-      'transition-duration': '1s',
-    };
-  } else if (idx === 0) {
-    return {
-      transform: `translate3d( calc(50vw - 474px + ${idx * 75}vw), 0px, 0px)`,
-      'transition-duration': '1s',
-      width: 948,
-      height: 606,
-    };
-  }
-  return {
-    // в левую сторону надо двигать на меньшее расстояние так как отсчет от увеличенной картинки
-    // 316 === 474 - (474 - 395)*2
-    transform: `translate3d( calc(50vw - 316px + ${idx * 75}vw), 0px, 0px)`,
-    'transition-duration': '1s',
-  };
-}
+const animationDuration = '0.25s';
 
 /* eslint-disable */
 class ExtendedCarousel extends Component {
@@ -79,45 +70,85 @@ class ExtendedCarousel extends Component {
         return { idx: i, imageId: children[i] };
       }),
       activeItem: 1,
+      offset: 0,
     };
   }
 
   slide = (e, direction) => {
     e.stopPropagation();
-    const { items, activeItem } = this.state;
+    const { items, activeItem, offset } = this.state;
     const idxDelta = direction ? 1 : -1;
     this.setState({
       items: items.map(({ idx, imageId }) => {
         return { idx: idx + idxDelta, imageId: imageId };
       }),
       activeItem: activeItem - idxDelta,
+      offset: offset - idxDelta * 100,
     });
   };
+
+  getWrapperTransformation(idx) {
+    const { offset } = this.state;
+    if (idx > 0) {
+      return {
+        transform: `translate3d(calc(-2rem - ${offset}%), 0, 0)`,
+        'transition-duration': animationDuration,
+        height: '80%',
+      };
+    } else if (idx === 0) {
+      return {
+        transform: `translate3d(${-offset}%, 0px, 0px)`,
+        'transition-duration': animationDuration,
+        height: '100%',
+        'z-index': 0,
+      };
+    } else {
+      return {
+        transform: `translate3d(calc(2rem - ${offset}%), 0, 0)`,
+        'transition-duration': animationDuration,
+        height: '80%',
+      };
+    }
+  }
+
+  getItemTransformation(idx) {
+    if (idx !== 0) {
+      return {
+        'justify-content': 'center',
+        'align-items': 'stretch',
+        flex: '1 0',
+        'margin-left': '0',
+        'margin-right': '0',
+      };
+    }
+  }
 
   render() {
     return (
       <RenderInBody style={modalWindowStyle}>
-        <BodyClassName className="carousel">
+        <BodyClassName className="modal-opened">
           <Carousel onClick={this.props.escHandler}>
-            <Wrapper>
-              {this.state.items.map(item => {
+            <ItemsWrapper>
+              {this.state.items.map(({ idx, imageId }) => {
                 return (
-                  <Item
-                    src={getImageUrl(item.imageId)}
-                    style={getTranformation(item.idx)}
-                    onClick={e => {
-                      if (item.idx > 0) {
-                        this.slide(e, false);
-                      } else if (item.idx < 0) {
-                        this.slide(e, true);
-                      } else {
-                        e.stopPropagation();
-                      }
-                    }}
-                  />
+                  <ItemWrapper style={this.getWrapperTransformation(idx)}>
+                    <Item
+                      style={this.getItemTransformation(idx)}
+                      src={getImageUrl(imageId)}
+                      onClick={e => {
+                        if (idx > 0) {
+                          this.slide(e, false);
+                        } else if (idx < 0) {
+                          this.slide(e, true);
+                        } else {
+                          e.stopPropagation();
+                        }
+                      }}
+                    />
+                  </ItemWrapper>
                 );
               })}
-            </Wrapper>
+            </ItemsWrapper>
             <Counter>
               {`${this.state.activeItem}/${this.state.items.length} Главный Фасад`}
             </Counter>
